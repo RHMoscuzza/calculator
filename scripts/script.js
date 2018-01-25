2f06bd049af73e923fb50ce2f050b6a17935e002
Beckah Moscuzza <rhmoscuzza@gmail.com>
2018-01-25 13:50:38 -0500

    working on JS

--- scripts/script.js
+++ scripts/script.js
@@ -17,5 +17,40 @@
 		resultNum,
 		operator;
 
-	
+	var setNum = function() {
+		if (resultNum) {
+			theNum = this.getAttribute("data-num");
+			resultNum = "";
+		}
+
+		viewer.innerHTML = theNum;
+	};
+
+	var moveNum = function() {
+		oldNum = theNum;
+		theNum = "";
+		operator = this.getAttribute("data-results", "");
+	};
+
+	var displayNum = function() {
+		oldNum = parseFloat(oldNum);
+		theNum = parseFloat(theNum);
+
+		switch (operator) {
+			case "plus":
+				resultNum = oldNum + theNum;
+				break;
+			case "minus":
+				resultNum = oldNum - theNum;
+				break;
+			case "multiply":
+				resultNum = oldNum * theNum;
+				break;
+			case "divide":
+				resultNum = oldNum / theNum;
+				break;
+			default:
+				resultNum = theNum;
+		}
+	}
 })
